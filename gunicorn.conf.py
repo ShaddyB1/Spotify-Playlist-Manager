71f690db5f8ce1a5133947c479ec44c73830cf37
ShadrackBoatengAddo <aaddoshadrack@gmail.com>
2024-11-05 01:01:06 -0800

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -7,12 +7,17 @@ backlog = 2048
 
 
 workers = multiprocessing.cpu_count() * 2 + 1
-worker_class = "gevent"  
+worker_class = "gevent"
+threads = 4
 worker_connections = 1000
-timeout = 300  
+timeout = 300
 keepalive = 2
 
 
+graceful_timeout = 300
+timeout = 300
+
+# Logging
 accesslog = "-"
 errorlog = "-"
 loglevel = "info"
@@ -21,6 +26,10 @@ loglevel = "info"
 proc_name = "spotify_playlist_manager"
 
 
+keyfile = None
+certfile = None
+
+# Server mechanics
 daemon = False
 pidfile = None
 umask = 0
@@ -28,3 +37,6 @@ user = None
 group = None
 tmp_upload_dir = None
 
+
+def child_exit(server, worker):
+    server.log.info("Worker exited: %s", worker.pid)
