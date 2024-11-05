8299d8a4a4897233933cb0d8356d75114f259908
ShadrackBoatengAddo <aaddoshadrack@gmail.com>
2024-11-04 20:57:11 -0800

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -3,11 +3,12 @@ import multiprocessing
 bind = "0.0.0.0:10000"
 workers = multiprocessing.cpu_count() * 2 + 1
 threads = 2
-timeout = 120
+timeout = 300  
 keepalive = 5
 max_requests = 1000
 max_requests_jitter = 50
-worker_class = "gevent"
-errorlog = "-"
+worker_class = "sync"  
 accesslog = "-"
 loglevel = "info"
+graceful_timeout = 300
+worker_connections = 1000
